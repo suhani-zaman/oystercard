38c0fdd83e5945123830f90aa6a4d6f1fd7bba01
blessingub <ubogublessing@yahoo.com>
2021-08-04 17:22:48 +0100

    Solution to chapter 11

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -1,35 +1,45 @@
 class Oystercard 
     MAXIMUM_BALANCE = 90 
     MINIMUM_BALANCE = 1
-    attr_reader :balance 
+    MINIMUM_FARE = 1
+    attr_reader :balance, :entry_station
 
 
     def initialize()
         @balance = 0
-        @maximum_balance = MAXIMUM_BALANCE 
-        @minimum_balance = MINIMUM_BALANCE
-        @in_journey = false
+        @in_journey = false 
+        @entry_station = ''
     end 
 
     def top_up(amount) 
-        raise 'Maximum limit reached' if (@balance + amount) > @maximum_balance
+        raise 'Maximum limit reached' if (@balance + amount) > MAXIMUM_BALANCE
         @balance += amount
     end 
 
-    def deduct(amount)
-        @balance -= amount 
-    end 
+     
 
-    def touch_in 
-        raise 'Insufficient funds' if @balance < @minimum_balance
+    def touch_in(station) 
+        @entry_station = station
+        raise 'Insufficient funds' if @balance < MINIMUM_BALANCE
         @in_journey = true 
     end 
 
-    def touch_out 
+    def touch_out
+        deduct(MINIMUM_FARE)
         @in_journey = false
+        @entry_station = nil
     end 
 
     def in_journey?
-        @in_journey
+        #!!@entry_station
+        @entry_station != nil
     end 
+    
+    
+
+    private
+    def deduct(amount)
+        @balance -= amount 
+    end
+
 end 
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -1,44 +1,57 @@
 require 'oystercard'
 
 describe Oystercard do 
-    card = Oystercard.new 
+    let (:station) {double :station} 
+
     it 'has a balance of zero' do 
-        expect(card.balance).to eq(0) 
+        expect(subject.balance).to eq(0) 
     end 
     
     describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
     end
     it 'should top_up the balance' do 
-        expect{ card.top_up 1}.to change{card.balance }.by 1
+        expect{ subject.top_up 1}.to change{subject.balance }.by 1
     end 
 
     it 'throws an error message when balance exceed limit' do 
         
         expect{ subject.top_up 1000}.to raise_error 'Maximum limit reached'
     end 
-    describe '#deduct' do
-    it { is_expected.to respond_to(:deduct).with(1).argument } 
-    end 
+    # describe '#deduct' do
+    # it { is_expected.to respond_to(:deduct).with(1).argument } 
+    # end 
 
-    it 'should deduct from the balance' do 
-        expect {subject.deduct 1}.to change{subject.balance}.by -1
-    end
+    # it 'should deduct from the balance' do 
+    #     expect {subject.deduct 1}.to change{subject.balance}.by -1
+    # end
 
     it 'checks if the card is in journey' do 
         subject.top_up(50)
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject.in_journey?).to eq true 
     end
 
     it 'checks if the card is not in journey' do 
         subject.top_up(50) 
-        subject.touch_in 
+        subject.touch_in(station) 
         subject.touch_out
         expect(subject.in_journey?).to eq false 
     end 
 
     it 'raise an error at touch in when there is an insufficient balance' do 
-        expect{ subject.touch_in}.to raise_error 'Insufficient funds'
+        expect{ subject.touch_in(station)}.to raise_error 'Insufficient funds'
+    end 
+
+    it 'checks if touch_out deducts money from the balance' do 
+        subject.top_up(5)
+        subject.touch_in(station)
+        expect{subject.touch_out}.to change{subject.balance}.by -1
+    end 
+
+    it 'stores the entry station after touch_in' do 
+        subject.top_up(5)
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq station
     end
 end 
