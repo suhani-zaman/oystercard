4ef4096a3a3e631363494d84fe20ce959855372a
blessingub <ubogublessing@yahoo.com>
2021-08-04 16:02:25 +0100

    Soltuion to chapter 9

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -1,11 +1,35 @@
 class Oystercard 
+    MAXIMUM_BALANCE = 90 
+    MINIMUM_BALANCE = 1
     attr_reader :balance 
 
-    def initialize  
+
+    def initialize()
         @balance = 0
+        @maximum_balance = MAXIMUM_BALANCE 
+        @minimum_balance = MINIMUM_BALANCE
+        @in_journey = false
     end 
 
     def top_up(amount) 
+        raise 'Maximum limit reached' if (@balance + amount) > @maximum_balance
         @balance += amount
-    end
+    end 
+
+    def deduct(amount)
+        @balance -= amount 
+    end 
+
+    def touch_in 
+        raise 'Insufficient funds' if @balance < @minimum_balance
+        @in_journey = true 
+    end 
+
+    def touch_out 
+        @in_journey = false
+    end 
+
+    def in_journey?
+        @in_journey
+    end 
 end 
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -12,4 +12,33 @@ describe Oystercard do
     it 'should top_up the balance' do 
         expect{ card.top_up 1}.to change{card.balance }.by 1
     end 
+
+    it 'throws an error message when balance exceed limit' do 
+        
+        expect{ subject.top_up 1000}.to raise_error 'Maximum limit reached'
+    end 
+    describe '#deduct' do
+    it { is_expected.to respond_to(:deduct).with(1).argument } 
+    end 
+
+    it 'should deduct from the balance' do 
+        expect {subject.deduct 1}.to change{subject.balance}.by -1
+    end
+
+    it 'checks if the card is in journey' do 
+        subject.top_up(50)
+        subject.touch_in
+        expect(subject.in_journey?).to eq true 
+    end
+
+    it 'checks if the card is not in journey' do 
+        subject.top_up(50) 
+        subject.touch_in 
+        subject.touch_out
+        expect(subject.in_journey?).to eq false 
+    end 
+
+    it 'raise an error at touch in when there is an insufficient balance' do 
+        expect{ subject.touch_in}.to raise_error 'Insufficient funds'
+    end
 end 
